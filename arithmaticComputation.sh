794708dcc5b5348229df8ca266f6f13e68e91cc7
prashikjaware <prashikjaware123@gmail.com>
2021-02-12 10:30:20 +0530

    Added: First, Second, Third and Fourth Computation of three numbers

--- arithmaticComputation.sh
+++ arithmaticComputation.sh
@@ -2,15 +2,18 @@
 
 echo "ENTER THE THREE NUMBERS: "
 read a b c
-
 result1=$(( $a + $b * $c ))
 
 result2=$(( $a * $b +$c ))
 
 result3=$(( $c + $a / $b ))
 
+result4=$(( $a % $b + $c ))
+
 echo $result1
 
 echo $result2
 
 echo $result3
+
+echo $result4
