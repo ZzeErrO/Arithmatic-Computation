4052d1d87cb2a2de2c01a6f04a2e80e22f99a841
prashikjaware <prashikjaware123@gmail.com>
2021-02-12 10:54:12 +0530

    Added: Store result from Dictionary to Array

--- arithmaticComputation.sh
+++ arithmaticComputation.sh
@@ -16,4 +16,10 @@ dict[1]=$result1
 dict[2]=$result2
 dict[3]=$result3
 dict[4]=$result4
-echo ${dict[@]}
+
+a[0]=$((dict[1]))
+a[1]=$((dict[2]))
+a[2]=$((dict[3]))
+a[3]=$((dict[4]))
+
+echo ${a[@]}
